95a30ccafe5b712268ce2e2975716af35d16ac4b
Steven Zhu <stevenzhu@Amandas-MBP.attlocal.net>
2020-04-20 23:04:48 -0500

    added optimization to drawing svg markers

--- resources/js/script.js
+++ resources/js/script.js
@@ -14,14 +14,14 @@ function initMap() {
     var icons = {
         paperPlane: {
             icon: {
-                url: '../FlightAPI/resources/images/paperplane.png',
+                url: '../genre_skies/resources/images/paperplane.png',
                 scaledSize: new google.maps.Size(20, 20),
                 anchor: new google.maps.Point(10, 10)
             }
         },
         whitePlane: {
             icon: {
-                url: '../FlightAPI/resources/images/whiteplane.png',
+                url: '../genre_skies/resources/images/whiteplane.png',
                 scaledSize: new google.maps.Size(20, 20),
                 anchor: new google.maps.Point(10, 10)
             }
@@ -40,16 +40,25 @@ function initMap() {
         anchor: new google.maps.Point(20, 20)
     };
 
-    // updateMarkers(plane);
-    // setInterval(updateMarkers, 30000, plane);
-    updateMarkers(icons['paperPlane'].icon);
-    setInterval(updateMarkers, 30000, icons['whitePlane'].icon);
+    var useSVG = true;
 
+    if (useSVG) {
+        updateMarkers(plane, gmap, markers);
+        setInterval(updateMarkers, 30000, plane, gmap, markers);
+    } else {
+        updateMarkers(icons['whitePlane'].icon, gmap, markers);
+        setInterval(updateMarkers, 30000, icons['whitePlane'].icon, gmap, markers);
+    }
+
+    // listener to set markers that are inbounds, and remove markers that are not
+    gmap.addListener('bounds_changed', function() {
+        setMapOnInBoundMarkers(gmap, markers);
+    });
 }
 
 
 // add marker to dictionary
-async function updateMarkers(icon) {
+async function updateMarkers(icon, gmap, markers) {
     let data = await fetch('https://opensky-network.org/api/states/all');
     // const plane1 = 'ab10df';
     // const plane2 = 'ae4a21';
@@ -71,7 +80,7 @@ async function updateMarkers(icon) {
                 var marker = markers[callsign];
 
                 // console.log('attempting to grab png');
-                // const png = document.querySelector(`[src="${marker.uniqueSrc}"]`);
+                // const png = document.querySelector(`[src="${marker.getIcon().url}"]`);
                 // console.log(`png: ${png}`);
                 // if (png) {
                 //     console.log('attempting to rotating');
@@ -83,28 +92,34 @@ async function updateMarkers(icon) {
                 currentIcon.rotation = rotation;
                 marker.setIcon(currentIcon);
 
-                // move marker recursively
-                moveMarker(100, (latitude - marker.getPosition().lat()) / 100, (longitude - marker.getPosition().lng()) / 100, marker);
+                // move marker recursively if position hasn't changed dramatically
+                var moves = 100;
+                var latDelta = latitude - marker.getPosition().lat();
+                var lngDelta = longitude - marker.getPosition().lng();
+                if (Math.abs(latDelta) > .5 || Math.abs(lngDelta) > .5)
+                    marker.setPosition(new google.maps.LatLng(latitude, longitude));
+                else
+                    moveMarker(moves, latDelta/moves, lngDelta/moves, marker);       
             } else {
                 if (onground == false && latitude != null) {
                     // icon.url += "#"+callsign;
-
                     var marker = new google.maps.Marker({
                         position: new google.maps.LatLng(latitude, longitude),
                         icon: icon,
-                        title: callsign,
-                        // uniqueSrc: icon.url
+                        title: callsign
                     });
                     markers[callsign] = marker;
-                    marker.setMap(gmap);
+                    // marker.setMap(gmap);
                 }
             }
         }
     }
-
     deleteOldMarkers(response);
+    setMapOnInBoundMarkers(gmap, markers);
 }
 
+
+// loop through response to delete plane from dict of markers, skip if the plane still exitst in the response
 function deleteOldMarkers(data) {
     var length = data.states.length;
     loop1:
@@ -121,9 +136,10 @@ function deleteOldMarkers(data) {
 // move marker smoothly by dividing the change into smaller parts
 function moveMarker(countdown, latDelta, lngDelta, marker) {
     marker.setPosition(new google.maps.LatLng(marker.getPosition().lat() + latDelta, marker.getPosition().lng() + lngDelta));
-    if (countdown > 0) {
-        setTimeout(moveMarker, 300, countdown - 1, latDelta, lngDelta, marker);
-    }
+    // waitTime * original countdown should always be less than time between api calls
+    var waitTime = 250;
+    if (countdown > 0) 
+        setTimeout(moveMarker, waitTime, countdown - 1, latDelta, lngDelta, marker);
 }
 
 // remove all markers from map
@@ -133,11 +149,20 @@ function clearMarkers() {
 
 // sets all maps in the dictionary
 function setMapOnAll(map) {
-    for (const key in markers) {
+    for (const key in markers)
         markers[key].setMap(map);
-    }
 }
 
+function setMapOnInBoundMarkers(gmap, markers) {
+    var mapCenter = gmap.getBounds();
+    for (const key in markers)
+        if (mapCenter.contains(markers[key].getPosition()))
+            markers[key].setMap(gmap);
+        else
+            markers[key].setMap(null);
+}
+
+// set marker to mew position test
 function drawMarkerTest() {
     var marker = new google.maps.Marker({
         position: { lat: 30.2672, lng: -97.7431 },
