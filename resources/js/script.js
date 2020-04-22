04d517908e4a287b3756a55130a2be1607937550
Steven Zhu <stevenzhu@Amandas-MBP.attlocal.net>
2020-04-22 18:57:00 -0500

    removed redundant function to draw markers, added better function comments

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,3 +1,6 @@
+/**
+ * Initializes google map
+ */
 function initMap() {
     var gmap;
     gmap = new google.maps.Map(document.getElementById('gmap'), {
@@ -48,7 +51,7 @@ function initMap() {
         setInterval(updateMarkers, 30000, icons['whitePlane'].icon, gmap, markers);
     }
 
-    // listener to set markers that are inbounds, and remove markers that are not
+    // listener to set markers that are inbounds, remove markers that are not when bounds change
     gmap.addListener('bounds_changed', function () {
         setMapOnInBoundMarkers(gmap, markers);
     });
@@ -56,12 +59,17 @@ function initMap() {
     var highlightedMarker = null;
     // add event listerner to flight search box to trigger searchForFlight()
     document.getElementById('flight-input').addEventListener('change', function () {
-        console.log(highlightedMarker);
         highlightedMarker = searchForFlight(this.value, gmap, markers, highlightedMarker);
     });
 }
 
-// add marker to dictionary
+/**
+ * Calls the open sky api and updates the location of all markers based on response
+ * 
+ * @param {Object} icon an SVG line projection or png reference 
+ * @param {Object} gmap google map
+ * @param {Object} markers dict to store markers with the callsign as key
+ */
 async function updateMarkers(icon, gmap, markers) {
     let data = await fetch('https://opensky-network.org/api/states/all');
     let response = await data.json();
@@ -71,7 +79,7 @@ async function updateMarkers(icon, gmap, markers) {
         var callsign = response.states[i][1];
 
         // check for callsign, ignoring null or empty
-        if (callsign != null && callsign != "        " && callsign != undefined) {
+        if (callsign != null && callsign != "        ") {
             var longitude = response.states[i][5];
             var latitude = response.states[i][6];
             var onground = response.states[i][8];
@@ -111,7 +119,12 @@ async function updateMarkers(icon, gmap, markers) {
     setMapOnInBoundMarkers(gmap, markers);
 }
 
-// loop through response to delete plane from dict of markers, skip if the plane still exitst in the response
+/**
+ * Loop through response to delete plane from dict of markers, skip if the plane still exist in the response
+ * 
+ * @param {Object} data A JSON object of the response from Open Sky API
+ * @param {Object} markers A dict of the Markers created in updateMarkers(); 
+ */
 function deleteOldMarkers(data, markers) {
     var length = data.states.length;
     loop1:
@@ -125,7 +138,14 @@ function deleteOldMarkers(data, markers) {
     }
 }
 
-// move marker smoothly by dividing the change into smaller parts
+/**
+ * Move marker smoothly by recursively iterating through the change in location in smaller parts
+ * 
+ * @param {int} countdown Number of remaining recursive calls
+ * @param {int} latDelta The change in latitude from marker's current latitude
+ * @param {int} lngDelta The change in longitude from marker's current longitude
+ * @param {Object} marker The marker that is being moved
+ */
 function moveMarker(countdown, latDelta, lngDelta, marker) {
     marker.setPosition(new google.maps.LatLng(marker.getPosition().lat() + latDelta, marker.getPosition().lng() + lngDelta));
     // waitTime * original countdown should never be more than time between api calls
@@ -134,7 +154,12 @@ function moveMarker(countdown, latDelta, lngDelta, marker) {
         setTimeout(moveMarker, waitTime, countdown - 1, latDelta, lngDelta, marker);
 }
 
-// set markers that are in bounds, remove those that are not
+/**
+ * Set markers that are in bounds, remove those that are not
+ * 
+ * @param {Object} gmap The google map object
+ * @param {Object} markers The dictionary that holds all the current markers
+ */
 function setMapOnInBoundMarkers(gmap, markers) {
     var mapCenter = gmap.getBounds();
     for (const key in markers)
@@ -144,18 +169,14 @@ function setMapOnInBoundMarkers(gmap, markers) {
             markers[key].setMap(null);
 }
 
-// set marker to mew position test
-function drawMarkerTest(gmap) {
-    var marker = new google.maps.Marker({
-        position: { lat: 30.2672, lng: -97.7431 },
-        icon: icons['whitePlane'].icon,
-        map: gmap
-    });
-    marker.setPosition({ lat: 30.7672, lng: -97.2431 });
-    marker.setMap(gmap);
-}
-
-// center map on and highlight flight
+/**
+ * Center map on and highlight new flight
+ * 
+ * @param {string} flightID The flight callsign passed in from input
+ * @param {Object} gmap The google map object
+ * @param {Object} markers The dict of markers currently tracked
+ * @param {Object} highlightedMarker The currently highlighted marker
+ */
 function searchForFlight(flightID, gmap, markers, highlightedMarker) {
     if (highlightedMarker != null) {
         var icon = highlightedMarker.getIcon();
@@ -165,23 +186,20 @@ function searchForFlight(flightID, gmap, markers, highlightedMarker) {
     }
 
     flightID = flightID.toUpperCase();
-    while (flightID.length != 8) {
+    while (flightID.length != 8)
         flightID += " ";
-    }
 
     var found = false;
     for (const key in markers) {
-
         if (key == flightID) {
             found = true;
             highlightedMarker = markers[key];
-            console.log(highlightedMarker);
-            var latlng = highlightedMarker.getPosition();
-            gmap.setCenter(latlng);
-            gmap.setZoom(10);
             var icon = highlightedMarker.getIcon();
             icon.fillColor = 'deepskyblue';
             highlightedMarker.setIcon(icon);
+            var latlng = highlightedMarker.getPosition();
+            gmap.setCenter(latlng);
+            gmap.setZoom(10);
             break;
         }
     }
@@ -192,7 +210,11 @@ function searchForFlight(flightID, gmap, markers, highlightedMarker) {
     return highlightedMarker;
 }
 
-// center map on user set location, currently not used due to no billing info on google account
+/**
+ * Center map on user set location, currently not used due to no billing info on google account
+ * 
+ * @param {Object} gmap The google map object
+ */
 function searchForLocation(gmap) {
     // Create the search box and link it to the UI element.
     var input = document.getElementById('floating-input');
